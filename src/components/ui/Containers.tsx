db76d1e81fd83a2d1b8ad4056f0d76930bb675c7
Hamed <hamedmahdizadeh@gmail.com>
2023-09-13 21:58:41 +0200

    Fix task list style bug

--- src/components/ui/Containers.tsx
+++ src/components/ui/Containers.tsx
@@ -69,10 +69,10 @@ export const StyledColumnDiv = styled.div<{ theme: Theme, basis: string, maxwidt
     margin-bottom: 5rem;
     border: 1px solid ${props => props.theme.border};
     padding: 1rem;
-    gap: .5rem,
-    display: flex,
-    flexWrap: wrap,
-    overflowY: auto,
+    gap: .5rem;
+    display: flex;
+    flex-wrap: wrap;
+    overflow-y: auto;
     @media (max-width: 767px) {
        max-width: unset;
     }
